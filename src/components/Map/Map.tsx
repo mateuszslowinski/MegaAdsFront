d76fa17b0c5505a64f39f65b28fc1991ea7d7785
Mateusz <nowe7246@gmail.com>
2022-06-02 15:43:38 +0200

    Create and implementation SearchContext.

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, {useContext, useEffect} from "react";
 import {MapContainer, Marker, TileLayer} from "react-leaflet";
 import '../../utils/fix-map-icons';
 
 import 'leaflet/dist/leaflet.css'
 import './Map.css';
+import {SearchContext} from "../../contexts/search.context";
 
 class Popup extends React.Component {
     render() {
@@ -12,13 +13,21 @@ class Popup extends React.Component {
 }
 
 export const Map = () => {
+
+    const {search} = useContext(SearchContext);
+
+    useEffect(()=> {
+
+    },[search]);
+
     return (
         <div className="map">
-            <MapContainer center={[50.2657152,18.9945008]} zoom={13}>
-             <TileLayer
-             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-             />
-                <Marker position={[50.2657152,18.9945008]}>
+            <h1>{search}</h1>
+            <MapContainer center={[50.2657152, 18.9945008]} zoom={13}>
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={[50.2657152, 18.9945008]}>
                     <Popup>
                         <h2>It.focus</h2>
                     </Popup>
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,18 +1,26 @@
-import React from "react";
+import React, {FormEvent, useContext, useState} from "react";
 import './Header.css';
 import {Btn} from "../common/Btn";
+import {SearchContext} from "../../contexts/search.context";
 
 export const Header = () => {
+
+    const {search, setSearch} = useContext(SearchContext);
+    const [inputVal,setInputVal] = useState(search);
+
+    const setSearchFromLocalState = (e:FormEvent) => {
+        e.preventDefault();
+        setSearch(inputVal);
+    };
+
     return (
-
-
         <header>
             <h1>Mega Ogłoesznia</h1>
             <Btn text="Dodaj ogłoszenie"></Btn>
-            <div className="search">
-                <input type="text"/>
+            <form className="search" onSubmit={setSearchFromLocalState}>
+                <input type="text" value={inputVal} onChange={e => setInputVal(e.target.value)}/>
                 <Btn text="Szukaj"></Btn>
-            </div>
+            </form>
         </header>
     )
 }
