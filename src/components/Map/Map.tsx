15e7815538b0cb9e02b122787cae1097a0041ea3
Mateusz <nowe7246@gmail.com>
2022-06-02 16:45:53 +0200

    Markers and map are finished.

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,37 +1,44 @@
-import React, {useContext, useEffect} from "react";
-import {MapContainer, Marker, TileLayer} from "react-leaflet";
+import React, {useContext, useEffect, useState} from "react";
+import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
 import '../../utils/fix-map-icons';
+import {SearchContext} from "../../contexts/search.context";
+import {SimpleAdEntity} from 'types';
 
 import 'leaflet/dist/leaflet.css'
 import './Map.css';
-import {SearchContext} from "../../contexts/search.context";
+import {SingleAd} from "./SingleAd";
 
-class Popup extends React.Component {
-    render() {
-        return null;
-    }
-}
 
 export const Map = () => {
-
     const {search} = useContext(SearchContext);
+    const [ads, setAds] = useState<SimpleAdEntity[]>([]);
 
-    useEffect(()=> {
+    useEffect(() => {
+        (async () => {
 
-    },[search]);
+            const res = await fetch(`http://localhost:3001/ad/search/${search}`);
+            const data = await res.json();
+
+            setAds(data);
+
+        })();
+    }, [search]);
 
     return (
         <div className="map">
-            <h1>{search}</h1>
+
             <MapContainer center={[50.2657152, 18.9945008]} zoom={13}>
                 <TileLayer
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <Marker position={[50.2657152, 18.9945008]}>
-                    <Popup>
-                        <h2>It.focus</h2>
-                    </Popup>
-                </Marker>
+                {ads.map(ad => (
+                    <Marker key={ad.id} position={[ad.lat, ad.lon]}>
+                        <Popup>
+                            <SingleAd id={ad.id}/>
+                        </Popup>
+                    </Marker>
+                ))
+                }
             </MapContainer>
         </div>
     )
